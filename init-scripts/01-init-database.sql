5f3b4c8a11f4ae3a88d0adf704756dd8ad322e8b
Krokko41 <77017681+Krokko41@users.noreply.github.com>
2026-02-24 01:52:51 +0100

    Update 01-init-database.sql

--- init-scripts/01-init-database.sql
+++ init-scripts/01-init-database.sql
@@ -4,11 +4,38 @@
 -- ============================================================================
 
 -- Opprett grunnleggende tabeller
+CREATE TABLE stasjon (
+    navn TEXT PRIMARY KEY
+);
 
+CREATE TABLE kunde (
+    id SERIAL PRIMARY KEY,
+    navn TEXT,
+    mobil TEXT
+);
+
+CREATE TABLE sykkel (
+    id SERIAL PRIMARY KEY,
+    status TEXT,
+    stasjon_navn TEXT REFERENCES stasjon(navn)
+);
+
+CREATE TABLE utleie (
+    id SERIAL PRIMARY KEY,
+    kunde_id INT REFERENCES kunde(id),
+    sykkel_id INT REFERENCES sykkel(id),
+    tid_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
 -- Sett inn testdata
+INSERT INTO stasjon (navn) VALUES ('Oslo S'), ('Aker Brygge');
 
+INSERT INTO kunde (mobil) VALUES ('90011222');
+
+INSERT INTO sykkel (status, stasjon_navn) VALUES ('Ledig', 'Oslo S');
+
+INSERT INTO utleie (kunde_id, sykkel_id) VALUES (1, 1);
 
 
 -- DBA setninger (rolle: kunde, bruker: kunde_1)
