a161c31fb5c2d2fecf2a4ebc70fe43b791521ae5
Krokko41 <77017681+Krokko41@users.noreply.github.com>
2026-02-26 19:14:47 +0100

    Update queries.sql

--- test-scripts/queries.sql
+++ test-scripts/queries.sql
@@ -4,5 +4,8 @@
 
 -- Kjør med: docker-compose exec postgres psql -h -U admin -d data1500_db -f test-scripts/queries.sql
 
+Oppgave5.1
+  SELECT * FROM sykkel;
+
 -- En test med en SQL-spørring mot metadata i PostgreSQL (kan slettes fra din script)
 select nspname as schema_name from pg_catalog.pg_namespace;
